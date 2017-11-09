875976e9ed72bcc8a4e235e637e4ba05e59b36cd
chenjiangquan <chenjiangquan123@gmail.com>
2017-11-09 18:15:16 +0000

    change name

--- pages/work/vip-interview.vue
+++ pages/work/vip-interview.vue
@@ -8,7 +8,7 @@
 import SpecificWork from '~/components/generic/SpecificWork'
 export default {
 	components: {SpecificWork},
-	workTitle: 'interview', // workTitle is required for each work
+	workTitle: 'vip-interview', // workTitle is required for each work
 	data () {
 		return {
 			text: ['JMAX MEDIA produces its own interview series with celebrities from any walk of life, notable people in the community and leading academics in their chosen fields. Recent episodes include interviews with the Law Lieutenant of the University of Sussex and the panel of the Chinese desk at PWC, with more to follow in the near future. Check back soon or follow us on YouTube (link) for regular updates.'],
